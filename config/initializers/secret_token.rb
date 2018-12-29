b8cee2dba5fb52e272ea54dfc15a7053633e46c7
Arnau <aerta7@gmail.com>
2018-12-29 15:13:53 +0100

    creat el projecte i agregat el status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kikired::Application.config.secret_token = '3a3b39ee6ac2ea9fe1bad52d82fc177fab37471dcc1d575acac8d598078dbdb45d912cd91e1806616e7c05037807254efce950b957fbe210cfd18650b6485a8b'
